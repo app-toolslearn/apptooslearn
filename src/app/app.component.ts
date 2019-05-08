6a7e4347244824c985dfe97404c35b5c284fbd05
kanjana <kanjana.taa39@gmail.com>
2019-05-08 20:28:58 +0700

    เเก้ภาษาไทยทุกหน้า

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,63 +1,62 @@
-import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav, MenuController, Img } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { timer } from 'rxjs/observable/timer';
+import { Component, ViewChild } from "@angular/core";
+import { Platform, Nav, MenuController, Img } from "ionic-angular";
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { timer } from "rxjs/observable/timer";
 import { Storage } from "@ionic/storage";
 
-import { FirstPage } from '../pages/first/first';
-import { LoginPage } from '../pages/login/login';
-import { HomePage } from '../pages/home/home';
-import { ReportPage } from '../pages/report/report';
-import { urlToNavGroupStrings } from 'ionic-angular/umd/navigation/url-serializer';
-import { LessonPage } from '../pages/lesson/lesson';
-import { TestPage } from '../pages/test/test';
-import { TabsPage } from '../pages/tabs/tabs';
-import { ForgetPasswordPage } from '../pages/forget-password/forget-password';
-import { MessagePage } from '../pages/message/message';
-import { EditProfilePage } from '../pages/edit-profile/edit-profile';
-import { RegisterPage } from '../pages/register/register';
+import { FirstPage } from "../pages/first/first";
+import { LoginPage } from "../pages/login/login";
+import { HomePage } from "../pages/home/home";
+import { ReportPage } from "../pages/report/report";
+import { urlToNavGroupStrings } from "ionic-angular/umd/navigation/url-serializer";
+import { LessonPage } from "../pages/lesson/lesson";
+import { TestPage } from "../pages/test/test";
+import { TabsPage } from "../pages/tabs/tabs";
+import { ForgetPasswordPage } from "../pages/forget-password/forget-password";
+import { MessagePage } from "../pages/message/message";
+import { EditProfilePage } from "../pages/edit-profile/edit-profile";
+import { RegisterPage } from "../pages/register/register";
 
 @Component({
-  templateUrl: 'app.html'
+  templateUrl: "app.html"
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
   [x: string]: any;
 
-  rootPage: any ;
+  rootPage: any;
   showSplash = true; // <-- show animation
 
   //pageArray menu Toggle
-  pages: Array<{ title: string, Component: any }>;
+  pages: Array<{ title: string; Component: any }>;
   activePage: any;
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
-    public menuCtrl: MenuController,private storage:Storage) {
-
-
+  constructor(
+    platform: Platform,
+    statusBar: StatusBar,
+    splashScreen: SplashScreen,
+    public menuCtrl: MenuController,
+    private storage: Storage
+  ) {
     this.pages = [
-      { title: 'Message', Component: MessagePage },
-      { title: 'Edit Profile', Component: EditProfilePage },
-
+      { title: "ส่งข้อความ", Component: MessagePage },
+      { title: "เเก้ไขโปรไฟล์", Component: EditProfilePage }
     ];
     this.activePage = this.pages[0];
-
-
 
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-       this.storage.get("user").then(data =>{
-         if (data != null) {
-          this.rootPage = LessonPage
-         } else {
-          this.rootPage = LoginPage
-         }
-
-       })
-      timer(1000).subscribe(() => this.showSplash = false)
+      this.storage.get("user").then(data => {
+        if (data != null) {
+          this.rootPage = LessonPage;
+        } else {
+          this.rootPage = LoginPage;
+        }
+      });
+      timer(1000).subscribe(() => (this.showSplash = false));
     });
   }
   openPage(page) {
@@ -70,10 +69,6 @@ export class MyApp {
   logoutClicked() {
     this.menuCtrl.close();
     this.nav.setRoot(LoginPage);
-    this.storage.clear()
-
-
+    this.storage.clear();
   }
-
 }
-
--- src/pages/edit-profile/edit-profile.ts
+++ src/pages/edit-profile/edit-profile.ts
@@ -1,14 +1,17 @@
-
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-import { Component } from '@angular/core';
-import { LoginPage } from '../login/login';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { TabsPage } from '../tabs/tabs';
-
+import {
+  IonicPage,
+  NavController,
+  NavParams,
+  AlertController
+} from "ionic-angular";
+import { Component } from "@angular/core";
+import { LoginPage } from "../login/login";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { TabsPage } from "../tabs/tabs";
 
 @Component({
-  selector: 'page-edit-profile',
-  templateUrl: 'edit-profile.html',
+  selector: "page-edit-profile",
+  templateUrl: "edit-profile.html"
 })
 export class EditProfilePage {
   public editpageForm;
@@ -19,28 +22,54 @@ export class EditProfilePage {
   repassword: any;
   passwordshow: boolean = false;
 
-
   firstPattern = "[a-zA-Z ]*";
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
   passwordPattern = "^[a-zA-Z0-9_-]{8,15}$";
   repasswordPattern = "^[a-zA-Z0-9_-]{8,15}$";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, myform: FormBuilder, private alertCtrl: AlertController) {
-    this.editpageForm = FormGroup
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    myform: FormBuilder,
+    private alertCtrl: AlertController
+  ) {
+    this.editpageForm = FormGroup;
     this.editpageForm = myform.group({
-      'firstname': [null, Validators.compose([Validators.required, Validators.pattern(this.firstPattern), Validators.minLength(4), Validators.maxLength(15)])],
+      firstname: [null, Validators.compose([Validators.required])],
       // 'lastname': [null, Validators.compose([Validators.required, Validators.pattern(this.firstPattern), Validators.minLength(4), Validators.maxLength(15)])],
 
-      'email': [null, Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
-      'password': [null, Validators.compose([Validators.required, Validators.pattern(this.passwordPattern), Validators.minLength(8), Validators.maxLength(15)])],
-      'repassword': [null, Validators.compose([Validators.required, Validators.pattern(this.repasswordPattern), Validators.minLength(8), Validators.maxLength(15)])],
-    })
+      email: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ])
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.passwordPattern),
+          Validators.minLength(8),
+          Validators.maxLength(15)
+        ])
+      ],
+      repassword: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.repasswordPattern),
+          Validators.minLength(8),
+          Validators.maxLength(15)
+        ])
+      ]
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+    console.log("ionViewDidLoad RegisterPage");
   }
-  
+
   ongotoregister() {
     if (this.editpageForm.password == this.editpageForm.repassword) {
       console.log(this.editpageForm.firstname);
@@ -49,19 +78,19 @@ export class EditProfilePage {
       console.log(this.editpageForm.password);
       console.log(this.editpageForm.repassword);
       let alert = this.alertCtrl.create({
-        title: ' Login Success',
-        subTitle: '',
+        title: " Login Success",
+        subTitle: "",
         buttons: [
           {
-            text: 'Cancel',
+            text: "Cancel",
             handler: data => {
-              console.log('Cancel clicked');
+              console.log("Cancel clicked");
             }
           },
           {
-            text: 'OK',
+            text: "OK",
             handler: data => {
-              console.log('Saved clicked');
+              console.log("Saved clicked");
               this.navCtrl.push(LoginPage);
             }
           }
@@ -70,15 +99,12 @@ export class EditProfilePage {
       alert.present();
     } else {
       let alert = this.alertCtrl.create({
-        title: ' Password ไม่ตรงกัน',
-        subTitle: 'โปรดกรอกให้ถูกต้อง',
-        buttons: ['OK']
-
+        title: " Password ไม่ตรงกัน",
+        subTitle: "โปรดกรอกให้ถูกต้อง",
+        buttons: ["OK"]
       });
       alert.present();
-
     }
-
   }
   backTologin() {
     this.navCtrl.push(LoginPage);
@@ -92,7 +118,6 @@ export class EditProfilePage {
     //   this.passwordshow = true;
     //   this.password = "password";
     // }
-
   }
   CloseeditPage() {
     this.navCtrl.push(TabsPage);
