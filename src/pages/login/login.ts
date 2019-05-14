dbffe5b0e9ba44451e2386a2b00d9045c947b3c6
kanjana <kanjana.taa39@gmail.com>
2019-05-14 19:12:51 +0700

    edit more app

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,17 +1,15 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
-import { RegisterPage } from '../register/register';
+import { Component } from "@angular/core";
+import { NavController, NavParams, AlertController } from "ionic-angular";
+import { RegisterPage } from "../register/register";
 import { TabsPage } from "../tabs/tabs";
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-import { ForgetPasswordPage } from '../forget-password/forget-password';
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
+import { ForgetPasswordPage } from "../forget-password/forget-password";
 import { Storage } from "@ionic/storage";
 import { Longin } from "../../service/login";
 
-
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html'
-
+  selector: "page-login",
+  templateUrl: "login.html"
 })
 export class LoginPage {
   [x: string]: any;
@@ -22,49 +20,60 @@ export class LoginPage {
 
   username: any;
   password: any;
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
   passwordPattern = "^[a-z0-9A-Z_-]{6,15}$";
-  constructor(public navCtrl: NavController, myform: FormBuilder, public navParams: NavParams, 
-    private alertCtrl: AlertController,private storage:Storage,private loginService :Longin) {
-    this.LoginForm = FormGroup
+  constructor(
+    public navCtrl: NavController,
+    myform: FormBuilder,
+    public navParams: NavParams,
+    private alertCtrl: AlertController,
+    private storage: Storage,
+    private loginService: Longin
+  ) {
+    this.LoginForm = FormGroup;
     this.LoginForm = myform.group({
-      'username': [null, Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
-      'password': [null, Validators.compose([Validators.required, Validators.pattern(this.passwordPattern)])],
-    })
-
-
-
+      username: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ])
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.passwordPattern)
+        ])
+      ]
+    });
   }
 
   gotoforgetpassword() {
     this.navCtrl.push(ForgetPasswordPage);
-
-
   }
   ongotoregister() {
     this.navCtrl.push(RegisterPage);
   }
   ongotorelogin() {
-
-
-    this.loginService.loginService(this.LoginForm.username,this.LoginForm.password).subscribe(data =>{
-      console.log(data)
-    if (data.length != 0) {      
-      this.storage.set('user',data)
-      this.navCtrl.push(TabsPage);
-      let status = {
-        status:0
-      } 
-    } else {
-      let alert = this.alertCtrl.create({
-        title: 'Username and Password',
-        subTitle: 'ไม่ถูกต้อง',
-        buttons: ['OK']
+    this.loginService
+      .loginService(this.LoginForm.username, this.LoginForm.password)
+      .subscribe(data => {
+        console.log(data);
+        if (data.length != 0) {
+          this.storage.set("user", data);
+          this.navCtrl.push(TabsPage);
+          let status = {
+            status: 0
+          };
+        } else {
+          let alert = this.alertCtrl.create({
+            title: "อีเมล์ เเละ รหัสผ่าน",
+            subTitle: "ไม่ถูกต้อง",
+            buttons: ["ตกลง"]
+          });
+          alert.present();
+        }
       });
-      alert.present();
-    }
-  })
-
   }
-
 }
--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,45 +1,76 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams, AlertController } from 'ionic-angular';
-import { LoginPage } from '../login/login';
-import { FormBuilder, Validators, FormGroup } from '@angular/forms';
-
-
+import { Component } from "@angular/core";
+import { NavController, NavParams, AlertController } from "ionic-angular";
+import { LoginPage } from "../login/login";
+import { FormBuilder, Validators, FormGroup } from "@angular/forms";
 
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+  selector: "page-register",
+  templateUrl: "register.html"
 })
 export class RegisterPage {
-
   public registerForm;
 
   firstname: any;
- 
+
   email: any;
   password: any;
   repassword: any;
   passwordshow: boolean = false;
 
-
   firstPattern = "[a-zA-Z ]*";
-  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
+  emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$";
   passwordPattern = "^[a-zA-Z0-9_-]{8,15}$";
   repasswordPattern = "^[a-zA-Z0-9_-]{8,15}$";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, myform: FormBuilder, private alertCtrl: AlertController) {
-    this.registerForm = FormGroup
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    myform: FormBuilder,
+    private alertCtrl: AlertController
+  ) {
+    this.registerForm = FormGroup;
     this.registerForm = myform.group({
-      'firstname': [null, Validators.compose([Validators.required, Validators.pattern(this.firstPattern), Validators.minLength(4), Validators.maxLength(15)])],
-      'email': [null, Validators.compose([Validators.required, Validators.pattern(this.emailPattern)])],
-      'password': [null, Validators.compose([Validators.required, Validators.pattern(this.passwordPattern), Validators.minLength(8), Validators.maxLength(15)])],
-      'repassword': [null, Validators.compose([Validators.required, Validators.pattern(this.repasswordPattern), Validators.minLength(8), Validators.maxLength(15)])],
-    })
+      firstname: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.firstPattern),
+          Validators.minLength(4),
+          Validators.maxLength(15)
+        ])
+      ],
+      email: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.emailPattern)
+        ])
+      ],
+      password: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.passwordPattern),
+          Validators.minLength(8),
+          Validators.maxLength(15)
+        ])
+      ],
+      repassword: [
+        null,
+        Validators.compose([
+          Validators.required,
+          Validators.pattern(this.repasswordPattern),
+          Validators.minLength(8),
+          Validators.maxLength(15)
+        ])
+      ]
+    });
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
+    console.log("ionViewDidLoad RegisterPage");
   }
- 
+
   ongotoregister() {
     if (this.registerForm.password == this.registerForm.repassword) {
       console.log(this.registerForm.firstname);
@@ -47,19 +78,19 @@ export class RegisterPage {
       console.log(this.registerForm.password);
       console.log(this.registerForm.repassword);
       let alert = this.alertCtrl.create({
-        title: ' Login Success',
-        subTitle: '',
+        title: "สำเร็จ",
+        subTitle: "",
         buttons: [
           {
-            text: 'Cancel',
+            text: "Cancel",
             handler: data => {
-              console.log('Cancel clicked');
+              console.log("Cancel clicked");
             }
           },
           {
-            text: 'OK',
+            text: "OK",
             handler: data => {
-              console.log('Saved clicked');
+              console.log("Saved clicked");
               this.navCtrl.push(LoginPage);
             }
           }
@@ -68,15 +99,12 @@ export class RegisterPage {
       alert.present();
     } else {
       let alert = this.alertCtrl.create({
-        title: ' Password ไม่ตรงกัน',
-        subTitle: 'โปรดกรอกให้ถูกต้อง',
-        buttons: ['OK']
-
+        title: " รหัสผ่าน ไม่ตรงกัน",
+        subTitle: "โปรดกรอกให้ถูกต้อง",
+        buttons: ["OK"]
       });
       alert.present();
-
     }
-
   }
   backTologin() {
     this.navCtrl.push(LoginPage);
@@ -90,7 +118,6 @@ export class RegisterPage {
     //   this.passwordshow = true;
     //   this.password = "password";
     // }
-
   }
   Closeregister() {
     this.navCtrl.push(LoginPage);
